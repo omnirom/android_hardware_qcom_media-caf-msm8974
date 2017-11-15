bb7902a5c54ebc709f909bfe4e2229d1d73a1813
Alexander Diewald <Diewi@diewald-net.com>
2017-11-15 21:30:52 +0100

    media-hal-8974: Place binaries below vendor dir.
    * Since the Oreo update, upstream demands that all vendor-specific
      binaries are placed in /vendor or /system/vendor. Hence, define
      LOCAL_VENDOR_MODULE := true for every module and use
      LOCAL_MODULE_RELATIVE_PATH instead LOCAL_MODULE_PATH that would
      prevent the use of LOCAL_VENDOR_MODULE.
    * This change allows to use QComs' selinux file labels.
    
    Change-Id: Ie493a133573c44a9086f751a2d107c182b0b5d76
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -94,6 +94,7 @@ LOCAL_C_INCLUDES        += $(LOCAL_PATH)/inc
 LOCAL_PRELINK_MODULE    := false
 LOCAL_MODULE            := libmm-omxcore
 LOCAL_MODULE_TAGS       := optional
+LOCAL_VENDOR_MODULE     := true
 LOCAL_SHARED_LIBRARIES  := liblog libdl
 LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
